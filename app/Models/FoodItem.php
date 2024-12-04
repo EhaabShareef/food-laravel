d2f74825968f9afe13c6e213f7ed082d1e3e4e1e
EhaabShareef <ehaab787shareef@gmail.com>
2024-12-04 12:59:34 +0500

    create models and install laravel sanctum

--- app/Models/FoodItem.php
+++ app/Models/FoodItem.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FoodItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'name', 'description', 'price', 'image_url', 'is_featured', 'is_active'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'food_item_id', 'rating', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function foodItem()
+    {
+        return $this->belongsTo(FoodItem::class);
+    }
 }
